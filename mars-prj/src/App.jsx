4faefd2929aef58ee933fd9bcbb4785377d27110
Fábio Tramasoli <fabio@tramasoli.com>
2026-03-15 11:43:21 -0300

    Translate intro to Portuguese and tidy code
    Update intro text to Portuguese. Also normalize code style
    (double quotes, semicolons, and minor formatting).

--- mars-prj/src/App.jsx
+++ mars-prj/src/App.jsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -16,10 +16,8 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
+          <h1>Apenas o começo</h1>
+          <p>Clique no contador abaixo para testar a aplicação ;)</p>
         </div>
         <button
           className="counter"
@@ -115,7 +113,7 @@ function App() {
       <div className="ticks"></div>
       <section id="spacer"></section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
